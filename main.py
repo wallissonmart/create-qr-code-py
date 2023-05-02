aaac16ee95e3ccf798cfb5465c105db0e61f2e8a
Wallisson Martins <wallissonmartins37@gmail.com>
2023-05-02 12:00:30 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import qrcode
+import json
+
+objeto = {
+    'id_venda': 302,
+    'valor_venda': 5.00,
+}
+
+qr = qrcode.QRCode(
+    version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+qr.add_data(json.dumps(objeto))
+qr.make(fit=True)
+imagem_qr = qr.make_image(fill_color='black', back_color='white')
+
+imagem_qr.save('qr_code_objeto.png')
